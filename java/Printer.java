559656c5255a1ad61bd52be477cb05edf05718c7
征途 <6031839+philiptong@users.noreply.github.com>
2018-02-10 14:35:50 +0000

    add a method to printString list

--- java/Printer.java
+++ java/Printer.java
@@ -1,5 +1,13 @@
+import java.util.List;
 
 public class Printer {
+	
+	public static void printStringList(List<String> strList) {
+		for(String str : strList) {
+			System.out.println(str);
+		}
+	}
+	
 
 	public static void printString(String str){
 		System.out.println(str);
