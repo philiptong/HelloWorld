e8e5935b7c77d60aecf61d049a5e8d1850e73d52
征途 <6031839+philiptong@users.noreply.github.com>
2018-02-10 15:18:36 +0000

    add printStringArray method

--- java/Printer.java
+++ java/Printer.java
@@ -2,12 +2,17 @@ import java.util.List;
 
 public class Printer {
 	
+	public static void printStringArray(String[] strArray) {
+		for(int i = 0; i < strArray.length; i ++) {
+			System.out.println(strArray[i]);
+		}
+	}
+	
 	public static void printStringList(List<String> strList) {
 		for(String str : strList) {
 			System.out.println(str);
 		}
 	}
-	
 
 	public static void printString(String str){
 		System.out.println(str);
