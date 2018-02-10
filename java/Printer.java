417ac250e2deb752c13ef891a160fbcfff7fae9c
征途 <6031839+philiptong@users.noreply.github.com>
2018-02-10 14:26:07 +0000

    change the printHello method to a general printing method for all String

---
+++ java/Printer.java
@@ -0,0 +1,14 @@
+
+public class Printer {
+
+	public static void printString(String str){
+		System.out.println(str);
+	}
+
+	
+	public static void main(String[] arg0) {
+		String str = "Hello World!!!";
+		Printer.printString(str);
+	}
+
+}
